7edf4bc7b172a70263b34ee8bc7dce480f252a77
Chmits <Chmits>
2016-08-03 17:14:42 +0200

    projet_soumis controller and functions done!

---
+++ controllers/projets_soumis.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+  if( $_SESSION['connected'] == "connected")
+  {
+	 include_once('../libraries/php/DAO/projectDAO.php');
+	 //include_once('../views/footer_view.php'); 
+	 $projectDao = new  ProjectDAO();
+	 $id_association=$_SESSION["id"];
+
+	 if($_SESSION["role"]=="association")
+	 {
+	 	 $result=$projectDao->getProjects_soumis();
+	 	 //var_dump($result);exit();
+	 }
+	
+	 include_once('../views/espace_projet_view.php');
+ }
+ else
+    header('Location:  ../index.php');      
+
+?>
--- libraries/php/DAO/projectDAO.php
+++ libraries/php/DAO/projectDAO.php
@@ -12,17 +12,16 @@ class projectDAO {
 		Db::close();
 
 	}
-	//recupère les projets
+	/* liste de tous les projets d'une association suisse */
 	public static function getProjects($id=0){
 
 		$sql="SELECT * FROM `t_project` where id_association_sw='$id' ;";
 		Db::open();
 		$result = Db::getRowList($sql);
 		Db::close();
-
-
         return $result ;
 	}
+	/* liste de tous les projets d'une association suisse mais affecté */
 	public static function getAffectedProjects($id=0){
 
 		$result = array();
@@ -34,18 +33,24 @@ class projectDAO {
 
         return $result ;
 	}
+		/* liste de tous les projets d'une association suisse mais affecté */
+	public static function getProjects_soumis(){
+		$result = array();
+		$sql="SELECT * FROM `t_project` where  status='soumis';";
+		Db::open();
+		$result = Db::getRowList($sql);
+		Db::close();
+        return $result ;
+	}
 
 	public static function editProject($id_association_sw=0,$id_team=0,$budget=0,$title=0,$document=0,$resume=0,$statut=0){
-
 		$sql = "UPDATE `t_project` set `budget` = '$budget',`title`='$title' , `document`='$document' , `resume`='$resume', `statut`='$statut' WHERE id_project = $id ;";
         DB::open();
         DB::updateQuery($sql);
         DB::close();
-
 	}
 	public static function deleteProject($id=0){
 		$sql="DELETE FROM `t_project` WHERE  `id_project` = $id;";
-		
 		DB::open();
         DB::deleteQuery($sql);
         DB::close();
