ba5eb1b74777d36ebbb32eda54f7aea5bb8a19fb
Chmits <Chmits>
2016-08-03 15:02:07 +0200

    correction des fonctions de projetDAO.php

--- libraries/php/DAO/projectDAO.php
+++ libraries/php/DAO/projectDAO.php
@@ -6,29 +6,30 @@ class ProjectDAO {
 
 	public static function insertProject($id_association_sw=0,$id_team,$budget=0,$title=0,$document=0,$resume=0,$statut=0){
 
-		//$sql="INSERT INTO t_project (`id_assoc_sw`, `title_2`, `budget`, `document`, `description`, `status`) VALUES  ($id_association, '$title', $budget, '$document', '$description', '$statut')";
-
-		//$sql=" NSERT INTO `t_project` (`id_project`, `id_assoc_sw`, `title_2`, `budget`, `document`, `description`, `status`) VALUES (NULL, '1', 'zz', '22', 'zdz', 'dzd', 'zd')";
-		
 		$sql="INSERT INTO t_project (`id_association_sw`, `id_team`, `budget`, `title`, `document`,`resume`, `status`) VALUES ($id_association_sw,$id_team,$budget, '$title', '$resume', '$document', 'status')";
-		
 		Db::open();
-	
 		Db::insertQuery($sql);
+		Db::close();
+
 	}
 	//recupère les projets
-	public static function getProjects(){
+	public static function getProjects($id=0){
+
+		$sql="SELECT * FROM `t_project` where id_association_sw='$id' ;";
+		Db::open();
+		$result = Db::getRowList($sql);
+		Db::close();
+
+
+        return $result ;
+	}
+	public static function getAffectedProjects($id=0){
 
 		$result = array();
-		$sql="SELECT * FROM `t_project` ;";
-
-        
-
+		$sql="SELECT * FROM `t_project` where id_association_sw='$id' and status='affected';";
 		Db::open();
-
-		
 		$result = Db::getRowList($sql);
-		
+		Db::close();
 
 
         return $result ;
